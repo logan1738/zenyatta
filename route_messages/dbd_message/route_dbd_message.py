b7313c03d45546b80b1d774099f7dce1a8a12b7c
Logan Stanford <loganstanford53@gmail.com>
2025-09-25 17:37:39 -0400

    remove empty lines

--- route_messages/dbd_message/route_dbd_message.py
+++ route_messages/dbd_message/route_dbd_message.py
@@ -3,7 +3,6 @@
 from dbd.base_commands.username import dbd_username_handler
 from safe_send import safe_send
 
-
 async def route_dbd_message(client, db, message, lower_message):
 
     if lower_message.startswith('!username'):

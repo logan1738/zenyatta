6a91e478f85b7c0a5c61b9fbb9381165f61b8bfd
Logan Stanford <loganstanford53@gmail.com>
2025-10-13 12:00:13 -0400

    ow-s7-semi-finals

--- automation/schedule_plan/add_week/add_week.py
+++ automation/schedule_plan/add_week/add_week.py
@@ -24,13 +24,14 @@ from time_helpers import year_month_day_to_datetime
 ADD_WEEK_CONFIG = {
 
     'start_day_info': {
-        'day': 6,
+        'day': 13,
         'month': 10,
         'year': 2025
     },
-    'season': 2,
+    'season': 7,
     'match_pairs': [
-        ['Deadlock', 'Hunters'],
+        ['Polar', 'Hunters'],
+        ['Olympians', 'Horizon'],
     ]
 
 }

4eb138b028c756df6d34e3b1a2b1ca17d06d1626
Logan Stanford <loganstanford53@gmail.com>
2025-10-29 17:18:43 -0400

    generate team info command

---
+++ command_handlers/league/generate_team_info.py
@@ -0,0 +1,44 @@
+
+
+from context.context_helpers import get_team_info_channel_from_context
+from helpers import get_constant_value, set_constant_value
+from safe_send import safe_send
+
+
+def context_messages_exist(team_info_context):
+
+    for team_name in team_info_context:
+        if team_info_context[team_name]['message_id'] != 0:
+            return True
+
+    return False
+    
+
+
+async def generate_team_info_handler(client, db, message, context):
+
+    team_info = get_constant_value(db, 'team_info')
+
+    if not context in team_info:
+        await safe_send(message.channel, 'Invalid context: '+context)
+        return
+    
+    team_info_context = team_info[context]
+
+    if context_messages_exist(team_info_context):
+        await safe_send(message.channel, f'Team info messages already exist for context {context}. Cannot generate new ones.')
+        return
+    
+    team_info_channel = get_team_info_channel_from_context(client, context)
+
+    for team_name in team_info_context:
+        team_info_text = team_name
+        team_info_message = await safe_send(team_info_channel, team_info_text)
+        team_info_context[team_name]['message_id'] = team_info_message.id
+
+    team_info[context] = team_info_context
+
+    set_constant_value(db, 'team_info', team_info)
+
+    await safe_send(message.channel, f'Team info messages generated for context {context}.')
+    

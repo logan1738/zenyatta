1d0b0bc20273a7ca1f1adb15524d7482dbed2262
Logan Stanford <loganstanford53@gmail.com>
2025-11-12 17:12:11 -0500

    mastermind survivor info

--- constants.py
+++ constants.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 VERSION = '1.11.59'
 
-DBD_DATA_VERSION = '1.2.7'
+DBD_DATA_VERSION = '1.2.8'
 
 OPEN_AI_TOKEN = os.getenv("OPEN_AI_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
--- dbd_perks/killers/individual_killer_stats/mastermind.py
+++ dbd_perks/killers/individual_killer_stats/mastermind.py
@@ -1,20 +1,41 @@
 
 
+from dbd_perks.items import SURVIVOR_ITEM_ADD_ON_NAMES, SURVIVOR_ITEM_NAMES
+from dbd_perks.offerings import SURVIVOR_OFFERING_NAMES
+from dbd_perks.survivor_perk_names import SURVIVOR_PERK_NAMES
+
+
 class MASTERMIND:
 
     NAME = "Mastermind"
 
-    TIER = 0
-
-    BANNED_SURVIVOR_PERKS = []
+    TIER = 3
+    
+    BANNED_SURVIVOR_PERKS = [
+        SURVIVOR_PERK_NAMES.LITHE
+    ]
 
     BANNED_SURVIVOR_PERK_COMBOS = []
 
-    SURVIVOR_ITEMS = []
-
     SURVIVOR_OFFERINGS = {
-        'offering1': [],
-        'offering2': [],
-        'offering3': [],
-        'offering4': []
+        'offering1': [SURVIVOR_OFFERING_NAMES.SHROUD_OF_BINDING],
+        'offering2': [SURVIVOR_OFFERING_NAMES.VIGOS_SHROUD],
+        'offering3': [SURVIVOR_OFFERING_NAMES.ANNOTATED_BLUEPRINT],
+        'offering4': [SURVIVOR_OFFERING_NAMES.BLOODIED_BLUEPRINT, SURVIVOR_OFFERING_NAMES.TORN_BLUEPRINT]
     }
+
+    SURVIVOR_ITEMS = [
+        {
+            'item': SURVIVOR_ITEM_NAMES.SPORT_FLASHLIGHT,
+            'add_ons': [
+                SURVIVOR_ITEM_ADD_ON_NAMES.BATTERY,
+                SURVIVOR_ITEM_ADD_ON_NAMES.LEATHER_GRIP,
+                SURVIVOR_ITEM_ADD_ON_NAMES.POWER_BULB,
+                SURVIVOR_ITEM_ADD_ON_NAMES.WIDE_LENS
+            ]
+        },
+        {
+            'item': SURVIVOR_ITEM_NAMES.CHINESE_FIRECRACKER,
+            'add_ons': []
+        }
+    ]

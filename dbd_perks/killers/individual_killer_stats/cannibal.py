eae5a64ab62541e73317b6de29fe9a948b6a740c
Logan Stanford <loganstanford53@gmail.com>
2025-11-14 15:22:50 -0500

    cannibal killer info

--- dbd_perks/killers/individual_killer_stats/cannibal.py
+++ dbd_perks/killers/individual_killer_stats/cannibal.py
@@ -1,7 +1,32 @@
 
 
 from dbd_perks.items import SURVIVOR_ITEM_ADD_ON_NAMES, SURVIVOR_ITEM_NAMES
-from dbd_perks.offerings import SURVIVOR_OFFERING_NAMES
+from dbd_perks.killers.killer_info import get_all_add_on_names
+from dbd_perks.offerings import KILLER_OFFERING_NAMES, SURVIVOR_OFFERING_NAMES
+
+
+class CANNIBAL_ADD_ON_NAMES:
+
+    CHAINSAW_FILE = "Chainsaw File"
+    SPARK_PLUG = "Spark Plug"
+    SPEED_LIMITER = "Speed Limiter"
+    VEGETABLE_OIL = "Vegetable Oil"
+    CHILLI = "Chilli"
+    HOMEMADE_MUFFLER = "Homemade Muffler"
+    KNIFE_SCRATCHES = "Knife Scratches"
+    LONG_GUIDE_BAR = "Long Guide Bar"
+    PRIMER_BULB = "Primer Bulb"
+    BEGRIMED_CHAINS = "Begrimed Chains"
+    GRISLY_CHAINS = "Grisly Chains"
+    SHOP_LUBRICANT = "Shop Lubricant"
+    THE_BEASTS_MARKS = "The Beast's Marks"
+    THE_GREASE = "The Grease"
+    AWARD_WINNING_CHILLI = "Award-Winning Chilli"
+    DEPTH_GAUGE_RAKE = "Depth Gauge Rake"
+    LIGHT_CHASIS = "Light Chasis"
+    RUSTED_CHAINS = "Rusted Chains"
+    CARBURETTOR_TUNING_GUIDE = "Carburettor Tuning Guide"
+    IRIDESCENT_FLESH = "Iridescent Flesh"
 
 
 class CANNIBAL:
@@ -43,8 +68,10 @@ class CANNIBAL:
 
     BANNED_KILLER_PERK_COMBOS = []
 
-    ALL_KILLER_ADD_ONS = []
+    ALL_KILLER_ADD_ONS = get_all_add_on_names(CANNIBAL_ADD_ON_NAMES)
 
-    BANNED_KILLER_ADD_ONS = []
+    BANNED_KILLER_ADD_ONS = [
+        CANNIBAL_ADD_ON_NAMES.LIGHT_CHASIS
+    ]
 
-    KILLER_OFFERINGS = []
+    KILLER_OFFERINGS = [KILLER_OFFERING_NAMES.CUT_COIN]

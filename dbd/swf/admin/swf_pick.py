3b350f2fa02a77476c798cd5571bb24283d5e2c4
Logan Stanford <loganstanford53@gmail.com>
2025-11-18 18:37:17 -0500

    fourth test user

--- dbd/swf/admin/swf_pick.py
+++ dbd/swf/admin/swf_pick.py
@@ -155,9 +155,11 @@ async def swf_pick_handler(client, db, message):
     extra_user_1 = user_exists(db, 592828437131952130) # Jacob
     extra_user_2 = user_exists(db, 706266168872140847) # Smelly Snail
     extra_user_3 = user_exists(db, 829457206034038856) # Kumbo
+    extra_user_4 = user_exists(db, 1031256636523827273) # Andy
     dbd_users.append(extra_user_1)
     dbd_users.append(extra_user_2)
     dbd_users.append(extra_user_3)
+    dbd_users.append(extra_user_4)
 
     if len(dbd_users) < NUM_SWF_PARTICIPANTS:
         num_dbd = len(dbd_users)

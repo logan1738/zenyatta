7339c69e31709a78fb59a45971dad1d49525c208
Logan Stanford <loganstanford53@gmail.com>
2025-11-21 15:08:01 -0500

    remove role from all users on cancel

--- dbd/swf/admin/swf_cancel.py
+++ dbd/swf/admin/swf_cancel.py
@@ -1,6 +1,7 @@
 
 
 import constants
+from discord_actions import get_guild
 from helpers import get_constant_value, set_constant_value
 from safe_send import safe_send
 
@@ -19,6 +20,12 @@ async def swf_cancel_handler(client, db, message):
     await swf_channel.send('Current SWF sign up has been cancelled by an admin.')
     
     set_constant_value(db, 'swf', swf_data)
+
+    guild = await get_guild(client)
+    swf_vc_access_role = guild.get_role(constants.SWF_VC_ACCESS_ROLE)
+
+    for member in swf_vc_access_role.members:
+        await member.remove_roles(swf_vc_access_role)
 
     await safe_send(message.channel, 'SWF sign up has been cancelled.')
     

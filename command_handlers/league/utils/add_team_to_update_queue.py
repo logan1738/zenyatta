d4fc1cd45d242a0b2af8c7de761483bf7a341cde
Logan Stanford <loganstanford53@gmail.com>
2025-12-04 14:06:03 -0500

    use set constant for teams to update

--- command_handlers/league/utils/add_team_to_update_queue.py
+++ command_handlers/league/utils/add_team_to_update_queue.py
@@ -1,6 +1,6 @@
 
 
-from helpers import get_constant_value
+from helpers import get_constant_value, set_constant_value
 
 
 def add_team_to_update_queue(db, context, team_name):
@@ -10,4 +10,5 @@ def add_team_to_update_queue(db, context, team_name):
 
     if team_name not in teams_in_context:
         teams_in_context.append(team_name)
-        db['constants'].update_one({'constant_name': 'teams_to_update'}, {'$set': {context: teams_in_context}})
+        teams_to_update_document[context] = teams_in_context
+        set_constant_value(db, 'teams_to_update', teams_to_update_document)
